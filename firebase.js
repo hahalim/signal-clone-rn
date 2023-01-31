e17c2623d88ee100e369cdc14b055d8455cf553a
hammad555 <hammadhalim555@gmail.com>
2023-02-01 00:01:20 +0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIG0wH--X9fJwaBFm-S7QF7I_qtWIAB98",
+  authDomain: "signal-clone-fffa1.firebaseapp.com",
+  projectId: "signal-clone-fffa1",
+  storageBucket: "signal-clone-fffa1.appspot.com",
+  messagingSenderId: "597791732660",
+  appId: "1:597791732660:web:64fa34b321e1cd21ba78e1",
+};
